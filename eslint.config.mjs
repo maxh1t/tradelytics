09407f6cbfec2ba5250d333248a550fe0582a13e
maxh1t <m4xh17@gmail.com>
2025-11-21 23:28:37 -0300

    cleaning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import pluginImport from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier'
+import unusedImport from 'eslint-plugin-unused-imports'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      prettier,
+      ['unused-imports']: unusedImport,
+      import: pluginImport,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/no-unescaped-entities': 'off',
+      'no-console': ['warn', { allow: ['error', 'warn', 'debug'] }],
+      'unused-imports/no-unused-imports': 'warn',
+      'import/no-duplicates': 'warn',
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+          pathGroups: [{ pattern: '@/**', group: 'internal', position: 'after' }],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
