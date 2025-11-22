268bbc50ae4be955e7ea356ee6e1fec351ed1de5
maxh1t <m4xh17@gmail.com>
2025-11-21 23:40:09 -0300

    add shadcn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/src/components/ui/button'
+import { Card } from '@/src/components/ui/card'
+
 export function Header() {
-  return <>Header</>
+  return (
+    <Card>
+      <Button>Header</Button>
+    </Card>
+  )
 }
